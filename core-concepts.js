4fb4b3e889625cf63183cf6df3e796bc042549e6
Hasib787 <mr.hasib45@gmail.com>
2021-02-20 22:18:28 +0600

    added a object

--- core-concepts.js
+++ core-concepts.js
@@ -1,1 +1,17 @@
-//variable: let, const
+// variable: let, const
+let money = 50;
+money =40;
+const name ='Hasib';
+
+let isGoing = true;
+
+ const person = {
+     name: 'Surovi',
+     age: 22,
+     city: 'Barishal'
+ }
+ const statement = `This person ${name} Love ${person.name}`;
+ console.log(statement);
+
+ //condition
+ 
