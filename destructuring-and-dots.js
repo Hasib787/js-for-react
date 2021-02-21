c0ff3ef1933027747d37b30c6ffb0a90a375967f
Hasib787 <mr.hasib45@gmail.com>
2021-02-21 22:54:57 +0600

    added a file and some destructuring array

---
+++ destructuring-and-dots.js
@@ -0,0 +1,7 @@
+//array destructuring 
+const numbers = [44, 18, 656, 12, 44, 5];
+// const x = numbers[0];
+// const y = numbers[1];
+// const [x, y] = [54, 787];
+const [x, y] = numbers;
+console.log(x, y);
