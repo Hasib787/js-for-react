6614a9b4613bb51593e06aa6f8d3b4f91259c406
Hasib787 <mr.hasib45@gmail.com>
2021-02-22 04:06:05 +0600

    added some js shortcut

--- js-shortcuts.js
+++ js-shortcuts.js
@@ -1,4 +1,5 @@
 let money = 150;
+let active= false;
 // let food;
 // if (money > 100) {
 //     food = 'biryani';
@@ -10,4 +11,28 @@ let money = 150;
 //ternary
 //condition ? true value : false value
 let food = money > 100 ? 'biryani' : 'alur vorta';
-console.log(food);
+// console.log(food);
+
+const cssClass = active ? 'active' : 'inactive';
+// console.log(cssClass);
+
+// active ? displayUser() : hideUser();
+
+// 
+const x = active && 5;
+const y = active || 10;
+
+// console.log(x);
+
+
+//convert string to number
+const number = +'45';
+console.log(number);
+
+//convert number to string
+const number2 = 55 + '';
+console.log(number2);
+
+
+
+
