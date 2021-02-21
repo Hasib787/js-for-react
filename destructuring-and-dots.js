f286b4bbccbb62adecaf7671238759817394dd0e
Hasib787 <mr.hasib45@gmail.com>
2021-02-21 22:59:15 +0600

    added some changes

--- destructuring-and-dots.js
+++ destructuring-and-dots.js
@@ -4,4 +4,13 @@ const numbers = [44, 18, 656, 12, 44, 5];
 // const y = numbers[1];
 // const [x, y] = [54, 787];
 const [x, y] = numbers;
-console.log(x, y);
+
+// console.log(x, y);
+
+function boxify(num1){
+    return [num1, num1+5];
+}
+
+const boxed = boxify(8);
+console.log(boxed);
+
