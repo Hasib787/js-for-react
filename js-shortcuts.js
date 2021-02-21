91c3e4d6c2edb2a0bc29db067b3f65de03b50e18
Hasib787 <mr.hasib45@gmail.com>
2021-02-21 23:41:46 +0600

    added a file name js shortcuts

---
+++ js-shortcuts.js
@@ -0,0 +1,13 @@
+let money = 150;
+// let food;
+// if (money > 100) {
+//     food = 'biryani';
+// }
+// else {
+//     food = 'dim vaji';
+// }
+
+//ternary
+//condition ? true value : false value
+let food = money > 100 ? 'biryani' : 'alur vorta';
+console.log(food);
